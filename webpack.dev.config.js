2028f75177c3e8de96b1df48148bddecd2a83c99
Jason Barr <jason@jasonsbarr.com>
2019-05-03 14:07:15 -0500

    Change webpack config for new build process

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,11 +1,17 @@
 const webpack = require('webpack');
+const htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    plugins: [
+        new htmlPlugin({
+            template: './src/html/index.html'
+        })
+    ],
     entry: {
         entry: __dirname + '/src/js/app.js'
     },
     output: {
-        path: __dirname + '/src/js',
+        path: __dirname + '/src',
         filename: 'app.bundle.js'
     }
 }
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,11 +1,17 @@
 const webpack = require('webpack');
+const htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    plugins: [
+        new htmlPlugin({
+            template: './src/html/index.html'
+        })
+    ],
     entry: {
-        entry: __dirname + '/src/js/app.bundle.js'
+        entry: __dirname + '/src/js/app.js'
     },
     output: {
-        path: __dirname + '/js',
-        filename: 'app.bundle.min.js'
+        path: __dirname + '/',
+        filename: 'app.bundle-[contenthash].min.js'
     }
 }
