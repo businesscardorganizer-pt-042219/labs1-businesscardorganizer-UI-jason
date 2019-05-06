e5e490e47c682420d03d73b2231f1bfaeb9c1564
Jason Barr <jason@jasonsbarr.com>
2019-05-06 11:58:03 -0500

    Add styles for testimonials and media cards

--- src/components/TestimonialCards/TestimonialCards.js
+++ src/components/TestimonialCards/TestimonialCards.js
@@ -13,17 +13,17 @@ class TestimonialCards extends LitElement {
             {
                 name: "John Doe",
                 image: "https://placeimg.com/100/100/people",
-                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi, molestiae dolorem a nostrum sed animi.",
+                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi.",
             },
             {
                 name: "John Doe",
                 image: "https://placeimg.com/100/100/people",
-                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi, molestiae dolorem a nostrum sed animi.",
+                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi.",
             },
             {
                 name: "John Doe",
                 image: "https://placeimg.com/100/100/people",
-                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi, molestiae dolorem a nostrum sed animi.",
+                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi.",
             },
         ];
     }
@@ -31,14 +31,17 @@ class TestimonialCards extends LitElement {
     render() {
         return html`
             <div class="cards">
-                <div class="card card-media">
-                    ${this.props.map(prop => {
-                        return html`
-                            <img class="card-img card-img-small card-media-img" src="${prop.image}" alt="${prop.name}">
-                            <p class card-text card-media-text>${prop.text} ~ ${prop.name}</p>
+                ${this.props.map(prop => {
+                    return html`
+                        <div class="card card-media">
+                            <img class="card-img card-img-thumb card-media-img" src="${prop.image}" alt="${prop.name}">
+                            <p class="card-text card-media-text">
+                                ${prop.text}<br>
+                                <span class="testimonial-name">~ ${prop.name}</span>
+                            </p>
+                        </div>
                         `;
                     })}
-                </div>
             </div>
         `;
     }
