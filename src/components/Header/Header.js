e127a687897314796adbe6dc296fc0122ef366e8
Jason Barr <jason@jasonsbarr.com>
2019-05-06 13:35:48 -0500

    Make header background full-width with wrapper

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,22 +5,24 @@ import '../NavPrimary/NavPrimary';
 class Header extends LitElement {
     render() {
         return html`
-            <header id="page-header" class="header">
-                <!-- Logo Img -->
-                <logo-img class="logo"></logo-img>
-                <h1 class="company-name">BCO</h1>
-                <!-- Primary Nav -->
-                <nav-primary></nav-primary>
-                <!-- buttons - secondary nav -->
-                <nav id="secondary" class="nav-btns">
-                    <a href="login" class="btn btn-primary-light">
-                        Log In
-                    </a>
-                    <a href="register" class="btn btn-secondary">
-                        Sign Up
-                    </a>
-                </nav>
-            </header>
+            <div id="header-wrapper" class="fw-wrapper">
+                <header id="page-header" class="header container">
+                    <!-- Logo Img -->
+                    <logo-img class="logo"></logo-img>
+                    <h1 class="company-name">BCO</h1>
+                    <!-- Primary Nav -->
+                    <nav-primary></nav-primary>
+                    <!-- buttons - secondary nav -->
+                    <nav id="secondary" class="nav-btns">
+                        <a href="login" class="btn btn-primary-light">
+                            Log In
+                        </a>
+                        <a href="register" class="btn btn-secondary">
+                            Sign Up
+                        </a>
+                    </nav>
+                </header>
+            </div>
         `;
     }
 
