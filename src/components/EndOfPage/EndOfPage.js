015ff52a5db8ca3d31f7915338cdbe18033a0ffb
Jason Barr <jason@jasonsbarr.com>
2019-05-06 21:09:08 -0500

    Add hero CTA at top of index page

--- src/components/EndOfPage/EndOfPage.js
+++ src/components/EndOfPage/EndOfPage.js
@@ -6,7 +6,7 @@ class EndOfPage extends LitElement {
     render() {
         return html`
             <section id="cta" class="section fw-wrapper cta">
-                <h2 class="cta-header">Call To Action</h2>
+                <h2 class="cta-heading">Call To Action</h2>
                 <p class="cta-text">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit mollitia quo nesciunt officia. Corrupti esse vitae debitis excepturi sint reprehenderit.
                 </p>
