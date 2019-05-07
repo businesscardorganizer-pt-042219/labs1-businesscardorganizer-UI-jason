a681329f6ee37184f78e1149efa9cf2e7d445f3c
Jason Barr <jason@jasonsbarr.com>
2019-05-07 01:01:56 -0500

    Update webpack configs to generate all HTML files from templates with updated dev & prod JS file locations

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,14 +4,27 @@ const htmlPlugin = require('html-webpack-plugin');
 module.exports = {
     plugins: [
         new htmlPlugin({
-            template: './src/html/index.html'
-        })
+            template: './src/html/index.html',
+            filename: '../index.html'
+        }),
+        new htmlPlugin({
+            template: './src/html/about.html',
+            filename: '../about.html'
+        }),
+        new htmlPlugin({
+            template: './src/html/features.html',
+            filename: '../features.html'
+        }),
+        new htmlPlugin({
+            template: './src/html/contact.html',
+            filename: '../contact.html'
+        }),
     ],
     entry: {
         entry: __dirname + '/src/js/app.js'
     },
     output: {
-        path: __dirname + '/',
+        path: __dirname + '/js',
         filename: 'app.bundle-[contenthash].min.js'
     }
 }
