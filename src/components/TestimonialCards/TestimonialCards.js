5ade9f299995a5408f7ced3301ef95603c6bdc54
Jason Barr <jason@jasonsbarr.com>
2019-05-06 20:46:17 -0500

    Add autofade functionality to testimonials and adjust positioning & z-index of abs + fixed positioned elements so testimonials scroll correctly on page

--- src/components/TestimonialCards/TestimonialCards.js
+++ src/components/TestimonialCards/TestimonialCards.js
@@ -4,6 +4,9 @@ class TestimonialCards extends LitElement {
     static get properties() {
         return {
             props: {type: Array},
+            elems: {type: NodeList},
+            active: {type: Number},
+            interval: {type: Number},
         };
     }
 
@@ -12,20 +15,62 @@ class TestimonialCards extends LitElement {
         this.props = [
             {
                 name: "John Doe",
-                image: "https://placeimg.com/100/100/people",
+                image: "https://randomuser.me/api/portraits/men/81.jpg",
                 text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi.",
             },
             {
-                name: "John Doe",
-                image: "https://placeimg.com/100/100/people",
-                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi.",
+                name: "Jane Doe",
+                image: "https://randomuser.me/api/portraits/women/29.jpg",
+                text: "Quisquam, possimus in esse, explicabo officiis fuga sequi perspiciatis praesentium culpa atque? Totam praesentium dolorem.",
             },
             {
-                name: "John Doe",
-                image: "https://placeimg.com/100/100/people",
-                text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laudantium corporis blanditiis commodi cum dolores nisi.",
+                name: "Bill Dye",
+                image: "https://randomuser.me/api/portraits/men/83.jpg",
+                text: "Cumque, maiores veritatis laudantium nulla natus mollitia similique dolores, voluptatum maxime eligendi animi tempora blanditiis!",
             },
         ];
+
+        setTimeout(() => {
+            this.elems = document.querySelectorAll('#testimonials .card');
+            this.active = 0;
+            this.elems[this.active].classList.add('active');
+            this.autofade();
+        }, 100);
+
+    }
+
+    autofade() {
+        this.interval = setInterval(() => {
+            this.showNext();
+
+            const cards = document.querySelector('#testimonials .cards');
+            cards.addEventListener('mouseenter', () => {
+                clearInterval(this.interval);
+            });
+
+            cards.addEventListener('touchstart', () => {
+                clearInterval(this.interval);
+            });
+
+            cards.addEventListener('mouseleave', () => {
+                this.showNext();
+                this.autofade();
+            });
+
+            cards.addEventListener('touchend', () => {
+                this.showNext();
+                this.autofade();
+            });
+        }, 5000);
+    }
+
+    showNext() {
+        this.elems.forEach(el => {
+            if (el.classList.contains('active')) el.classList.remove('active');
+        });
+
+        this.active = (this.active === this.elems.length - 1) ? 0 : this.active += 1;
+        this.elems[this.active].classList.add('active');
     }
 
     render() {
